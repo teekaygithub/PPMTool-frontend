927f1e85d3bd13c5e51216cfb1001353cf79371a
teekaygithub <tomoyuki.kato@yahoo.com>
2021-09-19 21:06:36 -0700

    Added routes with react-router-dom. Form to add projects. Button to open the add-project form.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import AddProject from './components/Project/AddProject';
 import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
