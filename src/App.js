f191b9e4212e543b91758060c0c5bcdacda56b56
teekaygithub <tomoyuki.kato@yahoo.com>
2021-09-19 20:11:17 -0700

    Dashboard and header components. Bootstrap for styling.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import Dashboard from "./components/Dashboard"
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
