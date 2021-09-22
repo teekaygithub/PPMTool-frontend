09af9d62eb36012be69ad4bfc0c64fd68f248bb5
teekaygithub <tomoyuki.kato@yahoo.com>
2021-09-21 20:39:12 -0700

    Implemented project addition. Action for displaying errors during project addition.

--- src/components/Project/AddProject.js
+++ src/components/Project/AddProject.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import PropTypes from "prop-types";
+import {connect} from 'react-redux';
+import {createProject} from '../../actions/projectActions';
 
 class AddProject extends Component {
     constructor() {
@@ -9,11 +12,21 @@ class AddProject extends Component {
             projectIdentifier: "",
             description: "",
             start_date: "",
-            end_date: ""
+            end_date: "",
+            errors: {}
         }
 
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    // life cycle hooks
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors
+            });
+        }
     }
 
     onChange(e) {
@@ -30,38 +43,44 @@ class AddProject extends Component {
             end_date: this.state.end_date
         };
 
-        console.log(newProject);
+        this.props.createProject(newProject, this.props.history);
     }
 
     render() {
+        const {errors} = this.state
         return (
-            <div className="project">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-8 m-auto">
-                            <h5 className="display-4 text-center">Create / Edit Project form</h5>
-                            <hr />
-                            <form onSubmit={this.onSubmit} >
-                                <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg " placeholder="Project Name" name="projectName" value={this.state.projectName} onChange={this.onChange} />
-                                </div>
-                                <div className="form-group">
-                                    <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID" name="projectIdentifier" value={this.state.projectIdentifier} onChange={this.onChange} />
-                                </div>
-                                <div className="form-group">
-                                    <textarea className="form-control form-control-lg" placeholder="Project Description" name="description" value={this.state.description} onChange={this.onChange} ></textarea>
-                                </div>
-                                <h6>Start Date</h6>
-                                <div className="form-group">
-                                    <input type="date" className="form-control form-control-lg" name="start_date" value={this.state.start_date} onChange={this.onChange} />
-                                </div>
-                                <h6>Estimated End Date</h6>
-                                <div className="form-group">
-                                    <input type="date" className="form-control form-control-lg" name="end_date" value={this.state.end_date} onChange={this.onChange} />
-                                </div>
+            <div>
+                <div className="project">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-8 m-auto">
+                                <h5 className="display-4 text-center">Create / Edit Project form</h5>
+                                <hr />
+                                <form onSubmit={this.onSubmit} >
+                                    <div className="form-group">
+                                        <input type="text" className="form-control form-control-lg " placeholder="Project Name" name="projectName" value={this.state.projectName} onChange={this.onChange} />
+                                        <p>{errors.projectName}</p>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID" name="projectIdentifier" value={this.state.projectIdentifier} onChange={this.onChange} />
+                                        <p>{errors.projectIdentifier}</p>
+                                    </div>
+                                    <div className="form-group">
+                                        <textarea className="form-control form-control-lg" placeholder="Project Description" name="description" value={this.state.description} onChange={this.onChange} ></textarea>
+                                        <p>{errors.description}</p>
+                                    </div>
+                                    <h6>Start Date</h6>
+                                    <div className="form-group">
+                                        <input type="date" className="form-control form-control-lg" name="start_date" value={this.state.start_date} onChange={this.onChange} />
+                                    </div>
+                                    <h6>Estimated End Date</h6>
+                                    <div className="form-group">
+                                        <input type="date" className="form-control form-control-lg" name="end_date" value={this.state.end_date} onChange={this.onChange} />
+                                    </div>
 
-                                <input type="submit" className="btn btn-primary btn-block mt-4" />
-                            </form>
+                                    <input type="submit" className="btn btn-primary btn-block mt-4" />
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -70,4 +89,16 @@ class AddProject extends Component {
     }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+    createProject: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    errors: state.errors
+});
+
+export default connect(
+    mapStateToProps, 
+    {createProject}
+) (AddProject);
