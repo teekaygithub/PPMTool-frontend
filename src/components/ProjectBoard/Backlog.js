7f7ee6f176b884919e3a8b4497378fd382c8a325
teekaygithub <tomoyuki.kato@yahoo.com>
2021-09-28 22:23:11 -0700

    Split the project tasks board into smaller components. Project board now loads the project tasks upon render.

---
+++ src/components/ProjectBoard/Backlog.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import ProjectTask from './ProjectTasks/ProjectTask';
+
+class Backlog extends Component {
+    render() {
+        const {project_task_prop} = this.props;
+        const tasks = project_task_prop.map(pt => (
+            <ProjectTask key={pt.id} project_task={pt} />
+        ));
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="card text-center mb-2">
+                            <div className="card-header bg-secondary text-white">
+                                <h3>TO DO</h3>
+                            </div>
+                        </div>
+                        {tasks}
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card text-center mb-2">
+                            <div className="card-header bg-primary text-white">
+                                <h3>In Progress</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card text-center mb-2">
+                            <div className="card-header bg-success text-white">
+                                <h3>Done</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Backlog;
