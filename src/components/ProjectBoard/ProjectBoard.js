89bb6aed5392bfa65d4ab3e0a505a311c82b35e8
teekaygithub <tomoyuki.kato@yahoo.com>
2021-09-29 23:50:52 -0700

    Error handling when trying to access non-existent project boards.

--- src/components/ProjectBoard/ProjectBoard.js
+++ src/components/ProjectBoard/ProjectBoard.js
@@ -8,6 +8,9 @@ import {getBacklog} from '../../actions/backlogActions';
 class ProjectBoard extends Component {
     constructor() {
         super();
+        this.state = {
+            errors: {}
+        }
     }
 
     componentDidMount() {
@@ -15,9 +18,43 @@ class ProjectBoard extends Component {
         this.props.getBacklog(id);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors
+            });
+        }
+    }
+
     render() {
         const {id} = this.props.match.params;
         const {project_tasks} = this.props.backlog;
+        const {errors} = this.props;
+
+        let BoardContent;
+
+        const boardAlgorithm = (errors, project_tasks) => {
+            if (project_tasks.length < 1) {
+                if (errors.projectNotFound) {
+                    return (
+                        <div className="alert alert-danger text-center" role="alert">
+                            {errors.projectNotFound}
+                        </div>
+                    );
+                } else {
+                    return (
+                        <div className="alert alert-info text-center" role="alert">
+                            No Project Tasks on this board
+                        </div>
+                    );
+                }
+            } else {
+                return <Backlog project_task_prop={project_tasks} />
+            }
+        };
+
+        BoardContent = boardAlgorithm(errors, project_tasks);
+
         return (
             <div className="container">
                 <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
@@ -25,7 +62,7 @@ class ProjectBoard extends Component {
                 </Link>
                 <br />
                 <hr />
-                <Backlog project_task_prop={project_tasks} />
+                {BoardContent}
             </div>
         );
     }
@@ -33,11 +70,13 @@ class ProjectBoard extends Component {
 
 ProjectBoard.propTypes = {
     backlog: PropTypes.object.isRequired,
-    getBacklog: PropTypes.func.isRequired
+    getBacklog: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-    backlog: state.backlog
+    backlog: state.backlog,
+    errors: state.errors
 })
 
 export default connect(mapStateToProps, {getBacklog})(ProjectBoard);
