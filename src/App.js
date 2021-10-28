2d4eec12fe74d57a8c63a1948f9a395abea95ca6
teekaygithub <tomoyuki.kato@yahoo.com>
2021-10-28 01:19:03 -0700

    Secured private routes so that non-authenticated users cannot access it and will be redirected

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AddProject from './components/Project/AddProject';
 import UpdateProject from './components/Project/UpdateProject';
 import ProjectBoard from './components/ProjectBoard/ProjectBoard';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from "./store";
 import AddProjectTask from './components/ProjectBoard/ProjectTasks/AddProjectTask';
@@ -17,6 +17,7 @@ import jwt_decode from 'jwt-decode';
 import setJWTToken from './securityUtils/setJWTToken';
 import {SET_CURRENT_USER} from './actions/types';
 import { logout } from './actions/securityActions';
+import SecuredRoute from './securityUtils/SecureRoute';
 
 const jwtToken = localStorage.jwtToken;
 
@@ -52,12 +53,14 @@ function App() {
           {
             // Private Routes
           }
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/addProject" component={AddProject} />
-          <Route exact path="/updateProject/:id" component={UpdateProject} />
-          <Route exact path="/projectBoard/:id" component={ProjectBoard} />
-          <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
-          <Route exact path="/updateProjectTask/:backlog_id/:pt_id" component={UpdateProjectTask} />
+          <Switch>
+            <SecuredRoute exact path="/dashboard" component={Dashboard} />
+            <SecuredRoute exact path="/addProject" component={AddProject} />
+            <SecuredRoute exact path="/updateProject/:id" component={UpdateProject} />
+            <SecuredRoute exact path="/projectBoard/:id" component={ProjectBoard} />
+            <SecuredRoute exact path="/addProjectTask/:id" component={AddProjectTask} />
+            <SecuredRoute exact path="/updateProjectTask/:backlog_id/:pt_id" component={UpdateProjectTask} />
+          </Switch>
         </div>
       </Router>
     </Provider>
